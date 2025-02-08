fb5b6fff27296ca4f5b4b9bdadbcfc511ee12304
jayadeep217 <gaddamjayadeep@gmail.com>
2025-02-08 14:40:44 +0000

    builtins

--- app/main.js
+++ app/main.js
@@ -8,10 +8,19 @@ const rl = readline.createInterface({
 
 function prompt() {
   rl.question("$ ", (input) => {
-    let command = input.split(" ")[0];
+    const builtins = ["echo", "exit", "type"];
+    let input_arr = input.split(" ");
+    let primary_command = input_arr[0];
     if (input === "exit 0") {
       exit(0);
-    } else if (command === "echo") {
+    } else if (primary_command === "type") {
+      let cmd = input_arr[1].trim();
+      if (builtins.includes(cmd)) {
+        console.log(`${cmd} is a shell builtin`);
+      } else {
+        console.log(`${cmd}: not found`);
+      }
+    } else if (primary_command === "echo") {
       console.log(`${input.replace("echo", "").trim()}`);
     } else {
       console.log(`${input}: command not found`);
@@ -19,4 +28,5 @@ function prompt() {
     prompt();
   });
 }
+
 prompt();
