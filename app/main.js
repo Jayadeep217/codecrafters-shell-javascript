63c9b482c7fd3dc9d433f3257f1c9919e7608864
jayadeep217 <gaddamjayadeep@gmail.com>
2025-02-08 13:00:50 +0000

    repl

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`);
-  rl.close();
-});
+function prompt() {
+  rl.question("$ ", (answer) => {
+    console.log(`${answer}: command not found`);
+    prompt();
+  });
+}
+prompt();
