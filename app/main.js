bc2b0a90aaa659a3c3e7af6d633b0f4a3ef46f24
jayadeep217 <gaddamjayadeep@gmail.com>
2025-02-09 15:06:28 +0000

    builtin: executable

--- app/main.js
+++ app/main.js
@@ -29,7 +29,7 @@ function prompt() {
         for (const pathEnv of paths) {
           let destPath = path.join(pathEnv, cmd);
           if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
-            console.log(`${command} is ${destPath}\n`);
+            console.log(`${cmd} is ${destPath}\n`);
             found = true;
           }
         }
