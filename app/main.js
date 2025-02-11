f40bc0cf6acfba10b0bb58f08177f38789631ed0
jayadeep217 <gaddamjayadeep@gmail.com>
2025-02-11 18:31:12 +0530

    refactor

--- app/main.js
+++ app/main.js
@@ -8,42 +8,70 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function exit_(exitCode) {
+  if (isNaN(exitCode) || exitCode < 0 || exitCode || 0) {
+    console.error("Invalid exit code");
+    return;
+  }
+  rl.close();
+  process.exit(exitCode);
+}
+
+function handleType(args){
+  const builtins = new Set(["exit", "type", "echo"]);
+  let found = false;
+  const cmd = args[1];
+  if (builtins.has(cmd)) {
+    console.log(`${cmd} is a shell builtin`);
+    found = true;
+  } else {
+    const paths = process.env.PATH.split(path.delimiter);
+    for (const pathEnv of paths) {
+      let destPath = path.join(pathEnv, cmd);
+      if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
+        console.log(`${cmd} is ${destPath}`);
+        found = true;
+      }
+    }
+    if (!found) {
+      console.log(`${restOfInput}: not found`);
+    }
+  }
+}
+
 function prompt() {
   rl.question("$ ", (input) => {
-    const builtins = new Set(["echo", "exit", "type"]);
-    const args = input.trim().split(/\s+/);
-    const command = args[0];
-    const restOfInput = args.slice(1).join(" ");
+    try {
 
-    if (command === "exit") {
-      const exitCode = parseInt(args[1]);
-      exit(exitCode);
-    } else if (command === "type") {
-      let found = false;
-      const cmd = args[1];
-      if (builtins.has(cmd)) {
-        console.log(`${cmd} is a shell builtin`);
-        found = true;
-      } else {
-        const paths = process.env.PATH.split(path.delimiter);
-        for (const pathEnv of paths) {
-          let destPath = path.join(pathEnv, cmd);
-          if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
-            console.log(`${cmd} is ${destPath}`);
-            found = true;
-          }
-        }
-        if (!found) {
-          console.log(`${restOfInput}: not found`);
-        }
+      if (!input.trim()) {
+        prompt();
+        return;
       }
-    } else if (command === "echo") {
-      console.log(restOfInput);
-    } else {
-      console.log(`${command}: command not found`);
+
+      const args = input.trim().split(/\s+/);
+      const command = args[0];
+      const restOfInput = args.slice(1).join(" ");
+
+      switch (command) {
+        case "exit":
+          exit_(Number(args[1]));
+          break;
+        case "type":
+          handleType(args);
+          break;
+        case "echo":
+          console.log(restOfInput);
+          break;
+          
+          default:
+            console.error(`${command}: command not found`);
+      }
+    } catch (err) {
+      console.error("An error occurred:", err.message);
     }
 
     prompt();
   });
 }
+
 prompt();
